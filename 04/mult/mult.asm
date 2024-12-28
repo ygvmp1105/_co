b5b1af803d1432746f4e321fc5ff5ec01063f088
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2024-12-28 15:48:18 +0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@0       // Load RAM[0] into D
+D=M
+@i       // i = 0 (loop index)
+M=0
+@res     // res = 0 (accumulator for result)
+M=0
+(loop)
+    @D   // If D == 0, exit loop
+    D=D-1
+    @end
+    D;JEQ
+
+    @1   // Add RAM[1] to res
+    A=M
+    D=M
+    @res
+    M=M+D
+
+    @i   // Increment i
+    M=M+1
+    @loop
+    0;JMP
+
+(end)
+    @res // Store result in RAM[2]
+    D=M
+    @2
+    M=D
+    (halt)
+    @halt
+    0;JMP
