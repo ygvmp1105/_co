914d7928ecb8f30a1e125982faf7253c16899b4b
ygvmp1105 <99934996+ygvmp1105@users.noreply.github.com>
2024-12-28 16:02:01 +0800

    Update Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,37 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// 填滿螢幕的程式
+(fill)
+    @SCR   
+    D=A    // D = 螢幕當前位址
+(fillLoop)
+    A=D    // 設定 A 為當前螢幕位址
+    M=-1   
+    D=D+1  
+    @endSCR
+    D-A    
+    D;JLT  
+    @KBD   
+    0;JMP
+
+// 清空螢幕的程式
+(clr)
+    @SCR  
+    D=A    
+(clrLoop)
+    A=D    
+    M=0    
+    D=D+1  
+    @endSCR 
+    D-A
+    D;JLT  
+    @KBD
+    0;JMP
+
+// 符號定義
+(SCR)
+@16384
+
+(endSCR)
+@24575
